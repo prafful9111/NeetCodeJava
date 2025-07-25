5682bd4143805d867b54e85a1553c62e2781168a
Prafful Choudhary <100062223+prafful9111@users.noreply.github.com>
2025-07-25 17:13:50 +0530

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -4,14 +4,16 @@
 import java.util.HashSet;
 
 class Solution1{
-    public boolean Duplicate(int[] nums){
-        HashSet<Integers> SeenNumbers = new HashSet<();
-        for (int num: nums){
-            if(SeenNumbers.contains(num)){
-                return true;
+    public boolean Duplicate(int[] nums){ //this is the return statement where we write after public what we need in output here it is boolean
+        HashSet<Integers> SeenNumbers = new HashSet<(); //this is how we use hashSet <Integers> for storing the num values
+        for (int num: nums){ //check for each num in the array 
+            if(SeenNumbers.contains(num)){ //if it contains numbers from hashset
+                return true; //then duplicate found 
             }
-            SeenNumbers.add(num);
+            SeenNumbers.add(num); //otherwise add the number in hashset
         }
-        return false;
+        return false; //return false no duplicate found 
     }
 }
+
+//Time Complexity = O(n) Space Complexity = O(n)
