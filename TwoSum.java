7af4076c479fd1a3552d4afd77111548f0c0a93a
Prafful Choudhary <100062223+prafful9111@users.noreply.github.com>
2025-07-25 17:33:42 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,14 +1,16 @@
+//Problem #3 to find the element and index of sum of two  number in target also called as target sum in short 
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for ( int i=0; i<nums.length; i++){
-            int remaining = target - nums[i];
+        Map<Integer, Integer> map = new HashMap<>(); //we will use the HashMap to store the array element and their index first
+        for ( int i=0; i<nums.length; i++){ //itrate through the array 
+            int remaining = target - nums[i]; //calculate the remaining number when substracted by target for each element
 
-            if (map.containsKey(remaining)){
-                return new int [] {map.get(remaining), i};
+            if (map.containsKey(remaining)){ //if the hashmap already contains the remaining element then 
+                return new int [] {map.get(remaining), i}; //return a new array with remaining number and its index
             }
-            map.put(nums[i], i);
+            map.put(nums[i], i); //if not then put that into map 
         }
-        return new int [] {};
+        return new int [] {}; //empty returm stateent to make sure the code does not conflict on run time.
     }
 }
