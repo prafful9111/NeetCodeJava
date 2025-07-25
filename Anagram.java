19002b779c31a856c1c7f33d658bb482642592a5
Prafful Choudhary <100062223+prafful9111@users.noreply.github.com>
2025-07-25 17:26:24 +0530

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,14 +1,17 @@
+//Problem#2 Given two strings, we have to fing they are anagram or not 
+// for this we will create a frequent array 
+
 class Anagram{
-  public boolean AnagramSolution(String s, String t){
-    if (s.length() != t.length()) return false;
-    int [] freq = new int[26];
-    for (int i=0; i<s.length(); i++){
-      freq[s.charAt(i) -'a']++;
-      freq[t.charAt(i) -'a']--;
+  public boolean AnagramSolution(String s, String t){ //return statement returning boolean to check if anagram is there or not
+    if (s.length() != t.length()) return false; //checking length if length is not same then no sense of checking 
+    int [] freq = new int[26]; //creating the new frequent integer array and store 26 into it
+    for (int i=0; i<s.length(); i++){ //itrating through the string and increment a of first nad decrement of another
+      freq[s.charAt(i) -'a']++; //incement for s
+      freq[t.charAt(i) -'a']--; //decrement for t
     }
-    for (int count: freq){
-      if (count != 0){
-        return false;
+    for (int count: freq){ //then we check the frequency count 
+      if (count != 0){ //if its zero then number is anagram
+        return false; //if not return false
       }
     }
     return true;
