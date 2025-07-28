d6dc8a04b8ddbd00e6408437b4a2a209aa6df3d3
Prafful Choudhary <100062223+prafful9111@users.noreply.github.com>
2025-07-28 11:40:23 +0530

    Create GroupAnagrams.java

---
+++ GroupAnagrams.java
@@ -0,0 +1,7 @@
+//Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+//Algorithm for this solution 
+// Cleared from the question we have to return list of strings in output
+// Check the length of the string if zero, return a empty list
+// If String if not empty then we create an Hashmap With String as key and List as its value
+// Then we initialize an integer variable named count 
+//
