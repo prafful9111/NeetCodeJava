af9e55980a1bd78966d9aa80494ebfabd3721ecf
Prafful Choudhary <100062223+prafful9111@users.noreply.github.com>
2025-07-28 12:52:44 +0530

    Update GroupAnagrams.java

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -1,4 +1,5 @@
 //Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+//We will be concurring this solution using HashMap, in which the key is the sorted string and the value is the similar strings to that relevant key which means the anagram of this keys.
 //Algorithm for this solution 
 // Cleared from the question we have to return list of strings in output
 // Check the length of the string if zero, return a empty list
