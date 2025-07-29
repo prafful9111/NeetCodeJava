828cf18de16587ce56d78f3c9719c54878ffc57a
Prafful Choudhary <100062223+prafful9111@users.noreply.github.com>
2025-07-29 11:31:26 +0530

    Update GroupAnagrams.java

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -1,8 +1,24 @@
 //Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
-//We will be concurring this solution using HashMap, in which the key is the sorted string and the value is the similar strings to that relevant key which means the anagram of this keys.
-//Algorithm for this solution 
-// Cleared from the question we have to return list of strings in output
-// Check the length of the string if zero, return a empty list
-// If String if not empty then we create an Hashmap With String as key and List as its value
-// Then we initialize an integer variable named count 
-//
+// The problem can be solved using the Hashmap where we store he string as value and its relevant anagrams into its value then we return those values in group which is asked in the question 
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap();
+        for (String s: strs){
+            int[] count = new int[26];
+            for(char c: s.toCharArray()){
+                count[c-'a']++;
+            }
+            StringBuilder keyBuilder = new StringBuilder();
+            for(int i: count){
+                keyBuilder.append('#');
+                keyBuilder.append(i);
+            }
+            String key = keyBuilder.toString();
+
+            map.computeIfAbsent(key, k-> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
